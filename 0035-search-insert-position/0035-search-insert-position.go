e6acfdb888373478cc244aa10d79c5f888d95b23
Dewi Oktaviani <oktaviani.dewi@gmail.com>
2022-10-25 14:41:48 +0700

    Time: 10 ms (25.16%), Space: 3 MB (8.42%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.go
@@ -0,0 +1,16 @@
+func searchInsert(nums []int, target int) int {
+	start := 0
+	end := len(nums) - 1
+
+	for start <= end {
+		mid := (start + end + 1) / 2
+		if target > nums[mid] {
+			start = mid + 1
+		} else if target < nums[mid] {
+			end = mid - 1
+		} else {
+			return mid
+		}
+	}
+	return start
+}
