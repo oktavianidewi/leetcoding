57ea28dabcfe4d6becad3fb24fd46d9c682f1cde
Dewi Oktaviani <oktaviani.dewi@gmail.com>
2022-10-25 15:18:50 +0700

    Time: 5 ms (74.73%), Space: 3 MB (8.42%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.go
+++ 0035-search-insert-position/0035-search-insert-position.go
@@ -1,16 +1,19 @@
 func searchInsert(nums []int, target int) int {
-	start := 0
-	end := len(nums) - 1
-
-	for start <= end {
-		mid := (start + end + 1) / 2
-		if target > nums[mid] {
-			start = mid + 1
-		} else if target < nums[mid] {
-			end = mid - 1
-		} else {
+	left, right := 0, len(nums)-1
+	for left < right {
+		mid := (left + right) / 2
+		if target == nums[mid] {
 			return mid
+		} else if target > nums[mid] {
+			left = mid + 1
+		} else {
+			right = mid - 1
 		}
+		// fmt.Printf("left %v, right %v, mid %v \n", left, right, mid)
+		// time.Sleep(1 * time.Second)
 	}
-	return start
+	if val := nums[left]; target > val {
+		return left + 1
+	}
+	return left
 }
